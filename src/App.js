ee5acc30a994709df3eb8e51c11502f111b404a2
Савелій Савёнак <1986developer@gmail.com>
2023-03-10 21:25:45 +0300

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/Manrope-VariableFont_wght.ttf 	new file:   src/Project.jsx 	new file:   src/Project.module.css 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/v201_188.png

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Project from './Project';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+      <div><h1 className="headerText">Проект Minecraft серверов</h1></div> <div><h1 className="headerText">SpectruMine.com</h1></div>
+      </div>
+      <div className='linearGrad'/>
+      <div className="projects">
+      <h1 className="hserv">Minecraft сервера</h1>
+        <div className="projectsIn">
+        <Project title={"title"}></Project>
+        <Project title={"title"}></Project>
+        </div>
+      </div>
+      <div className='linearGradRev'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Project.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Project.module.css";
+
+const Project = function (title) {
+    console.log(classes.mainBlock)
+    return(
+        <div className={classes.mainBlock}>
+            
+        </div>
+    )
+}
+export default Project;
