bddaff4d77cbc391a8ba07a78c13235b906f1bd4
Савелій Савёнак <1986developer@gmail.com>
2023-03-11 23:32:41 +0300

    update project 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './appComponents/App';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
     <App />
 );
