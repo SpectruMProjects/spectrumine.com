de15e39eef3897fedc2245f2da7c0871847088f5
Савелій Савёнак <1986developer@gmail.com>
2023-03-12 16:15:50 +0300

    Button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import BigBabyText from './Components/BigBabyText';
+import Button from './Components/Button';
 import Containter from './Components/Container';
 import Header from './Components/Header';
 
 function App() {
+  let func = () => {
+  }
   return (
     <div className="App">
       <Header></Header>
       <div className='flexitems bgintro' style={{minHeight: "100dvh"}}>
       <div>
-        <BigBabyText size={"5dvw"} margin={"0 10dvw"}>Добро пожаловать на платформу лицензионных игровых серверов Minecraft</BigBabyText>
+        <BigBabyText size={"4dvw"} margin={"0 10dvw"}>Добро пожаловать на платформу лицензионных игровых серверов Minecraft</BigBabyText>
+        <div className='centerbutton'>
+        <Button onClick={func} width={"20dvw"} height={"10dvh"}>Перейти по ссылке</Button>
+        </div>
       </div>
       </div>
       <div className='flexitems' style={{height: "100dvh", flexGrow: 4}}>
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,6 @@
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props: any) => {
     return(<div className={classes.head}><h1 className={classes.h1}>SpectruMine.com</h1><h1 className={classes.h1} style={{textAlign: "right"}}>Платформа серверов Minecraft</h1></div>);
 }
 export default Header;
