8a430d7858f6858b818e5feaacf57fe3ec23dfe3
Gleb <M20XV@yandex.ru>
2023-03-30 14:34:35 +0300

    auth update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,32 @@
+import { Button, Card, Typography } from 'antd'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthPageState } from '../store'
+
+
+const rootStyle = { 
+  height: '100%',
+  width: '100%', 
+  display: 'flex', 
+  alignItems: 'center', 
+  justifyContent: 'center' 
+}
 
 export default function Profile() {
-  const [lol] = useState("lol")
+  const user = useAuthPageState(s => s.user)
+  const nav = useNavigate()
+  if (!user) 
+    return <div 
+      style={rootStyle}>
+      <Card style={{ height: 'fit-content' }}>
+      <Typography.Title>ТЫ {'[маленькая губка]!'}</Typography.Title>
+      <Button type='primary' onClick={() => nav('/auth')}>
+        СТАТЬ [БОЛЬШАЯ ШИШКА]
+      </Button>
+      </Card>
+    </div>
   
-  return <div>
-    Profile
-    {lol}
-  </div>
+  return <div style={rootStyle}><Card>
+    <Typography.Title>ТЫ {`[${user}]`}</Typography.Title>
+  </Card></div>
 }
