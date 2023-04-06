a0cf89477d9198597973eb3471213630b88f9f29
Gleb <M20XV@yandex.ru>
2023-04-06 19:58:34 +0300

    404 text align center

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 export default function NotFound() {
   return (
     <div
-      style={{ padding: '2em' }} 
+      style={{ padding: '2em', textAlign: 'center' }} 
       className={styles['centered-page']}>
       <Typography.Title>
         Страница {window.location.pathname} не найдена
