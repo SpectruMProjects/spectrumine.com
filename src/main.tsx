053397d453dab588feed412f3d3845991b879439
Gleb <M20XV@yandex.ru>
2023-04-15 16:43:39 +0300

    off dev mod for production

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,18 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+if (import.meta.env.DEV) {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  )  
+} else {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  )
+}
