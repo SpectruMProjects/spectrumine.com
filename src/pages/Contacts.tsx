e37204d146242e96fd7a5d4afbf10ed9fe76e9f0
Gleb <M20XV@yandex.ru>
2023-04-16 13:37:51 +0300

    update contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,23 +1,31 @@
 import { List, Typography } from 'antd'
 import styles from './styles.module.css'
 import { useSetPageTitle } from '@/hooks'
+import { MailOutlined } from '@ant-design/icons'
 
 const messengers = [
   {
-    name: 'Telegram',
-    link: 'Lisoveily',
-    linkSrc: 'https://t.me/Lisoveily',
+    name: 'Telegram - ',
+    link: ' Наша группа',
+    linkSrc: 't.me/spectrumine',
     icon: <img
       src='/icons/telegram.svg' 
       alt='telegram logo' />
   },
   {
-    name: 'Discord',
-    link: 'Lisoveily',
-    linkSrc: 'https://t.me/Lisoveily',
+    name: 'Discord - ',
+    link: 'Наш канал',
+    linkSrc: 'https://discord.gg/w2Ks8HFPDJ',
     icon: <img
       src='/icons/discord.svg' 
       alt='discord logo' />  
+  }
+]
+
+const emails = [
+  {
+    email: 'spectruminesup@gmail.com ',
+    description: ' - Поддержка'
   }
 ]
 
@@ -49,7 +57,21 @@ export default function Contacts() {
               {messenger.link}
             </Typography.Link>
           </div>)}/>
-      
+
+      <List
+        className={styles['contacts-page__emails']}
+        header={
+          <p className={styles['contacts-page__emails__title']}>
+            <MailOutlined /> Почта
+          </p>}
+        dataSource={emails}
+        renderItem={(messenger => 
+          <div
+            key={messenger.email}
+            className={styles['contacts-page__emails__contact']}>
+            <Typography.Text>{messenger.email}</Typography.Text>
+            <Typography.Text>{messenger.description}</Typography.Text>
+          </div>)}/>
     </div>
   )
 }
