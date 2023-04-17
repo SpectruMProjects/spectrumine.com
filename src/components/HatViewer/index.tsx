76989de89975d54e2f25051dc780d1fc3c2720aa
Gleb <M20XV@yandex.ru>
2023-04-17 16:28:24 +0300

    add textures loader

--- src/components/HatViewer/index.tsx
+++ src/components/HatViewer/index.tsx
@@ -6,9 +6,10 @@ import {
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
-  DirectionalLight
+  DirectionalLight,
+  Color
 } from 'three'
-import { OBJLoader, OrbitControls } from '@/core'
+import { OBJLoader, OrbitControls, MTLLoader } from '@/core'
 
 interface Props {
   width?: number
@@ -26,27 +27,56 @@ export default function HatViewer({ width = 400, height = 400 }: Props) {
     const scene = new Scene()
     const camera = new PerspectiveCamera(75)
     const renderer = new WebGLRenderer()
-    const light = new DirectionalLight('white', 2)
+
+    const topLight = new DirectionalLight('wheat')
+    topLight.position.set(0, 100, 0)
+
+    const bottomLight = new DirectionalLight('gray')
+    bottomLight.position.set(0, -100, 0)
+
+    const keyLight = new DirectionalLight(new Color('hsl(30, 100%, 75%)'), 1)
+    keyLight.position.set(-100, 0, 100)
+
+    const fillLight = new DirectionalLight(
+      new Color('hsl(240, 100%, 75%)'),
+      0.75
+    )
+    fillLight.position.set(100, 0, 100)
+
+    const backLight = new DirectionalLight(0xffffff, 1)
+    backLight.position.set(100, 0, -100).normalize()
+
+    scene.add(topLight)
+    scene.add(bottomLight)
+    scene.add(keyLight)
+    scene.add(fillLight)
+    scene.add(backLight)
 
     camera.position.x = 2
-    light.position.set(3, 5, 7)
 
     renderer.setClearColor(0xffffff, 0)
     renderer.setSize(w, h)
-    scene.add(light)
 
     ref.current.replaceWith(renderer.domElement)
 
     const controls = new OrbitControls(camera, renderer.domElement)
 
     const objLoader = new OBJLoader()
-    loadObj(objLoader, '/models/test_hat.obj').then((hat) => {
-      if (!isWork) return
-      scene.add(hat)
+    const mtlLoader = new MTLLoader()
 
-      const { x, y, z } = hat.position
-      controls.target.set(x, y, z)
-      controls.update()
+    loadMTL(mtlLoader, '/textures/test_hat.mtl').then((mtl) => {
+      if (!isWork) return
+      mtl.preload()
+      objLoader.setMaterials(mtl)
+
+      loadObj(objLoader, '/models/test_hat.obj').then((hat) => {
+        if (!isWork) return
+        scene.add(hat)
+
+        const { x, y, z } = hat.position
+        controls.target.set(x, y, z)
+        controls.update()
+      })
     })
 
     let draw = () => {
@@ -83,6 +113,12 @@ function createHatObj() {
 
 function loadObj(loader: OBJLoader, url: string) {
   return new Promise<Mesh>((r, rj) => {
+    loader.load(url, r, () => {}, rj)
+  })
+}
+
+function loadMTL(loader: MTLLoader, url: string) {
+  return new Promise<any>((r, rj) => {
     loader.load(url, r, () => {}, rj)
   })
 }
--- src/core/threejs/index.js
+++ src/core/threejs/index.js
@@ -1,2 +1,3 @@
 export { OrbitControls } from './OrbitControls'
 export { OBJLoader } from './OBJLoader'
+export { MTLLoader } from './MTLLoader'
