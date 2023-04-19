374e1721fabae8e45ff930ac4b521fb339519dd3
Савелій Савёнак <1986developer@gmail.com>
2023-04-19 12:35:22 +0300

    Added models on store

--- src/components/Products/Hat.tsx
+++ src/components/Products/Hat.tsx
@@ -2,11 +2,14 @@ import { HatProduct } from '@/models'
 import styles from './styles.module.css'
 import { Skeleton, Typography } from 'antd'
 import { useNavigate } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
 
 interface Props {
   hat: HatProduct
   onClick?: () => void
 }
+
+const AsyncHatViewer = lazy(() => import('@/components/HatViewer'))
 
 export default function Hat({ hat, onClick }: Props) {
   const nav = useNavigate()
@@ -22,10 +25,9 @@ export default function Hat({ hat, onClick }: Props) {
           alt={`${hat.name} preview`}
         />
       ) : (
-        <Skeleton.Image
-          style={{ width: 150, height: 150 }}
-          className={styles['hat__preview']}
-        />
+        <Suspense>
+          <AsyncHatViewer url={hat.gLTFUrl} />
+        </Suspense>
       )}
 
       <div className={styles['hat__info']}>
