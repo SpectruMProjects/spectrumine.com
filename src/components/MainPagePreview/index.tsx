a5b0231f90e17c58995b634774c60b21b7782717
Gleb <M20XV@yandex.ru>
2023-05-15 10:40:14 +0300

    removes timer from main

--- src/components/MainPagePreview/index.tsx
+++ src/components/MainPagePreview/index.tsx
@@ -1,9 +1,6 @@
-import { useTimer } from '@/hooks'
 import styles from './styles.module.css'
 
 export default function MainPagePreview() {
-  const time = useTimer(1683997200000)
-
   return (
     <div className={styles['main-page-preview']}>
       <div className={styles['header']}>
@@ -13,9 +10,6 @@ export default function MainPagePreview() {
       <div className={styles['info']}>
         Добро пожаловать на платформу лицензионных игровых серверов Minecraft
         <br />
-        <span style={{ fontSize: '0.7em' }}>
-          {time !== '0:0:0:0' && `До запуска Hardcore сервера осталось ${time}`}
-        </span>
       </div>
 
       <div style={{ flex: 1 }} />
