2404548874b6a68769bc257d42180eeae28fd23e
Савелій Савёнак <1986developer@gmail.com>
2023-05-18 10:58:59 +0300

    new file:   public/Manrope-VariableFont_wght.ttf 	new file:   public/bg.png 	deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	new file:   public/title.png 	modified:   src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	new file:   src/App.tsx 	new file:   src/Menu.tsx 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='mainblock'>
+      <div className='innertitle'>
+        <img src='title.png' alt='spectrumine' className='titleimg' />
+      </div>
+      <div className='bigtext'>
+        <p>Платформа лицензионных серверов Minecraft</p>
+      </div>
+      <div className='innertitle'>
+        <div className='button'>
+          <div>
+            К серверам
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Render(){
+    return (<div>
+        
+    </div>)
+}
