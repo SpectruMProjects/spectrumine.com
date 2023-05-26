ab39448a1d1c123465be05c2a40a9dd2dc7fd981
Saveliy Savenak <1986developer@gmail.com>
2023-05-26 19:24:24 +0300

    modified:   src/locales/en.ts 	modified:   src/locales/index.ts 	modified:   src/locales/ru.ts

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,20 +1,21 @@
 import { Locale } from './locale'
 import ru from './ru'
+import en from './en'
 
 export * from './locale'
 
 export default {
-  locales: ['ru'] as const,
+  locales: ['ru', 'en'] as const,
   localesNames: {
-    ru: 'Русский'
-    // en: 'English'
+    ru: 'Русский',
+    en: 'English'
   },
 
-  ru
+  ru,
 
-  // get en(): Promise<Locale> {
-  //   return import('./en').then((e) => e.default)
-  // }
+  get en(): Promise<Locale> {
+      return import('./en').then((e) => e.default)
+  }
 
   // get be() {
   //   return import('./be')
